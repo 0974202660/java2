e0a51deb910d93ca27b5f630da012e4dfd40e34d
minhdq8 <50552420+minhdq8@users.noreply.github.com>
2019-06-01 11:02:59 +0700

    Add files via upload

---
+++ J2Demo4/src/demo5/KhachHang.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo5;
+
+/**
+ *
+ * @author Admin
+ */
+
+// Bước 1 lớn 
+public class KhachHang {
+    public String ID;
+    public String name;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNgaysinh() {
+        return ngaysinh;
+    }
+
+    public void setNgaysinh(String ngaysinh) {
+        this.ngaysinh = ngaysinh;
+    }
+
+    public String getDienthoai() {
+        return dienthoai;
+    }
+
+    public void setDienthoai(String dienthoai) {
+        this.dienthoai = dienthoai;
+    }
+    public String ngaysinh;
+    public String dienthoai;
+}
