69ec0a595382cf0557c886ab212de0351fc17eff
minhdq8 <50552420+minhdq8@users.noreply.github.com>
2019-06-11 10:43:05 +0700

    Add files via upload

---
+++ Form/Test1.java
@@ -0,0 +1,22 @@
+package j2testgiuaky;
+
+
+import java.io.Serializable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author minhdq
+ */
+public class Test1 implements Serializable{
+    public String name;
+    public boolean gender = false;
+    public String skill;
+    public String nganh;
+    
+}
